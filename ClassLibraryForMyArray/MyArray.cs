dc208e06d05bd409988142656a72733f05c76129
Andrew <andrew0erm2@gmail.com>
2022-01-21 23:07:51 +0300

    Created ClassLibrary and ConsoleApp projects

---
+++ ClassLibraryForMyArray/MyArray.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArrayLibrary
+{
+    public class MyArray
+    {
+        public int Lenght { get; }
+        private double[] ThisArray;
+        public MyArray(double[] thisArray)
+        {
+            ThisArray = thisArray;
+            Lenght = thisArray.Length;
+        }
+
+        public double this[int index]
+        {
+            get => ThisArray[index];
+            set => ThisArray[index] = value;
+        }
+
+        public static int MetodsForMyArray(MyArray array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Lenght; i++)
+            {
+                if (array[i] < 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static int MetodsForMyArray(MyArray array, int enteredNumber)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Lenght; i++)
+            {
+                if (i > enteredNumber)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static int MetodsForMyArray(MyArray array, double value)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Lenght; i++)
+            {
+                if (array[i] > value)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static MyArray operator *(MyArray array1, MyArray array2)
+        {
+            double[] arr;
+            if (array1.Lenght > array2.Lenght)
+            {
+                arr = new double[array1.Lenght];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (i < array2.Lenght)
+                    {
+                        arr[i] = array1[i] * array2[i];
+                    }
+                    else
+                    {
+                        arr[i] = array1[i];
+                    }
+                }
+            }
+            else if (array1.Lenght < array2.Lenght)
+            {
+                arr = new double[array2.Lenght];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (i < array1.Lenght)
+                    {
+                        arr[i] = array1[i] * array2[i];
+                    }
+                    else
+                    {
+                        arr[i] = array2[i];
+                    }
+                }
+            }
+            else
+            {
+                arr = new double[array1.Lenght];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = array1[i] * array2[i];
+                }
+            }
+
+            return new MyArray(arr);
+        }
+    }
+}
---
+++ Variant7/Program.cs
@@ -0,0 +1,53 @@
+﻿using MyArrayLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ using System.Threading.Tasks;
+
+namespace Variant7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Array A:");
+                MyArray A = new MyArray(CreateMyArray());
+                
+                Console.WriteLine("Array B:");
+                MyArray B = new MyArray(CreateMyArray());
+                
+                Console.WriteLine("Array C:");
+                MyArray C = new MyArray(CreateMyArray());
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error: " + error.Message);
+            }
+        }
+        static double[] CreateMyArray()
+        {
+            Console.Write("Enter count elements of array: ");
+            if (!int.TryParse(Console.ReadLine(), out int count))
+            {
+                throw new Exception("Entered value is used be integer");
+            }
+
+            double[] Array = new double[count];
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write($"Enter {i + 1} element of array:");
+                if (!double.TryParse(Console.ReadLine(), out double value))
+                {
+                    throw new Exception("Entered value is used be double");
+                }
+                Array[i] = value;
+            }
+
+            return Array;
+        }
+    }
+}
