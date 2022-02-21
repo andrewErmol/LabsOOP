0ddca64a8a9754d96e8bcb4417d761e39343c80e
andrewErmol <andrew0erm2@gmail.com>
2022-02-21 14:25:02 +0300

    Create library class

---
+++ HumanLibrary/Human.cs
@@ -0,0 +1,10 @@
+﻿namespace HumanLibrary
+{
+    public abstract class Human
+    {
+        public abstract string Info();
+        public string surname;
+        public DateTime YearOfBirth;
+        public string status;
+    }
+}
---
+++ HumanLibrary/Humans/Working.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanLibrary.Humans
+{
+    public class Working : Human
+    {
+    }
+}
